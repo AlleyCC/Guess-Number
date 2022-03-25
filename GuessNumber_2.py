c6cfa5147f2acb1255641540fbae8d8bc9c7f134
AlleyChang <yifanchang0602@gmail.com>
2022-03-25 14:34:28 +0800

    2ndVersion

---
+++ GuessNumber_2.py
@@ -0,0 +1,14 @@
+import random
+#先載入模組
+r = random.randint (1, 100)
+while True:
+	num = input ('猜一個數字:')
+	num = int(num)
+	if num == r:
+		print ('你猜對了!')
+		break
+	elif num > r: 
+		print('不對~再小一點')
+	elif num < r:
+		print ('不對~再大一點')
+	
