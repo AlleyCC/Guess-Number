44fc8ba82faebca89da9c03e9c1ac99bffcd508d
AlleyChang <yifanchang0602@gmail.com>
2022-03-25 14:56:07 +0800

    final version

---
+++ GN_4.py
@@ -0,0 +1,20 @@
+start = input ('請決定隨機數字開始值')
+end = input ('請決定隨機數字結束值')
+start = int(start)
+end = int(end)
+import random
+#先載入模組
+r = random.randint (start, end)
+count = 0 #count=計數
+while True:
+	count += 1 #count = count +1
+	num = input ('猜一個數字:')
+	num = int(num)
+	if num == r:
+		print ('你猜對了!')
+		break
+	elif num > r: 
+		print('不對~再小一點')
+	elif num < r:
+		print ('不對~再大一點')
+	print ('這是你猜的第', count, '次')
